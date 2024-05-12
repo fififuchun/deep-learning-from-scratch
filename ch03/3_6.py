c325475a269e91aa00a7de02d64f15ded6eda3c5
fififuchun <windkindstar20030205@gmail.com>
2024-05-12 20:22:27 +0900

    Mnistデータのインポートに成功

--- ch03/3_6.py
+++ ch03/3_6.py
@@ -1,11 +1,15 @@
 import sys, os
+import numpy as np  # type: ignore
+import pickle
+
 sys.path.append(os.pardir)
 from dataset.mnist import load_mnist
 
-(x_train, t_train), (x_test, t_test)= \
-    load_mnist(flatten= True, normalize= False)
+(x_train, t_train), (x_test, t_test) = load_mnist(
+    flatten=True, normalize=False, one_hot_label=False
+)
 
-print(x_train.shapes)
+print(x_train.shape)
 print(t_train.shape)
 print(x_test.shape)
-print(t_test.shape )
+print(t_test.shape)
